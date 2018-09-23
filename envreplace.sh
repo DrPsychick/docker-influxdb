8911be132269c68b0dbca3ad7b74a3866bd532dc
DrPsychick <github@drsick.net>
2018-09-23 16:23:43 +0200

    add howto to readme (#2)
    * add license
    * add readme with 3-step howto (tested)

--- envreplace.sh
+++ envreplace.sh
@@ -21,7 +21,7 @@ if [ "$1" = "--test" ]; then
   for v in $(set |grep ^${conf_var_prefix}|sed -e 's/^\('${conf_var_prefix}'[^=]*\).*/\1/' |sort |tr '\n' ' ' ); do
     [ -z "$v" ] && continue
     value=$(eval echo -n \""\$$v"\")
-    echo -e "$v=\"$value\""
+    echo -e "$v='$value'"
   done
   exit 0
 fi

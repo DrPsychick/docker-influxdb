20f0abdbd5e25187090ad36656d7ab22510cdfed
DrPsychick <drpsychick@drsick.de>
2020-08-02 17:01:41 +0200

    fix deploy.sh (#7)

--- deploy.sh
+++ deploy.sh
@@ -3,7 +3,7 @@
 # required Travis-ci secret variables (all other variables to be set in .travis.yml)
 # DOCKER_PASS, DOCKER_USER
 
-version_cmd="influxd version | sed -e 's/.* v\([0-9.]*\) .*/\1/'"
+version_cmd="influxd version | grep '^InfluxDB' | sed -e 's/.* v\([0-9.]*\) .*/\1/'"
 
 # login to docker
 echo $DOCKER_PASS | docker login -u $DOCKER_USER --password-stdin &> /dev/null || exit 1
@@ -24,7 +24,7 @@ if [ "$TRAVIS_BRANCH" = "master" -a "$TRAVIS_PULL_REQUEST" = "false" ]; then
     # build single platform and load it into local docker repository, so we can launch it and determine version
     docker buildx build --progress plain --platform linux/amd64 --build-arg ALPINE_VERSION=$ALPINE_VERSION \
       -t $IMAGE --load .
-    export VERSION=$(docker run --rm -it $IMAGE $version_cmd)
+    export VERSION=$(docker run --rm $IMAGE $version_cmd)
 
     # build for all platforms again and push with correct version tag
     docker buildx build --progress plain --platform $PLATFORMS --build-arg ALPINE_VERSION=$ALPINE_VERSION \
